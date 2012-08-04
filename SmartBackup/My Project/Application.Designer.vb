e93a33e3289bb2aeead4a7d3cb06f2566d341545
Pedro Silva <pemiolsi@gmail.com>
2012-08-04 17:27:50 +0100

    Moved DLL and the reference to Krypton Toolkit into the project folder, and changed the Assembly name.

--- SmartBackup/My Project/Application.Designer.vb
+++ SmartBackup/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.EasyBackup.frm_main
+            Me.MainForm = Global.SmartBackup.frm_main
         End Sub
     End Class
 End Namespace
